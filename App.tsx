f02f4786da26d9cbec97de20f58c65006bc1be1c
Prayag Joshi <prayagjoshi263@gmail.com>
2024-03-01 15:28:55 +0530

    Removed unused imports

--- App.tsx
+++ App.tsx
@@ -1,15 +1,11 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
-import HomeScreen from './src/screens/HomeScreen';
 import CustomDrawer from './src/navigation/CustomDrawer';
-
-interface Props {}
 
 const Stack = createNativeStackNavigator();
 
-const App = (props: Props) => {
+const App = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator
@@ -24,5 +20,3 @@ const App = (props: Props) => {
 };
 
 export default App;
-
-const styles = StyleSheet.create({});
