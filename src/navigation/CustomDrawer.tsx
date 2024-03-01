e0db0b5621ccd5499c4d12e4207a0b27e5db3462
Prayag Joshi <prayagjoshi263@gmail.com>
2024-03-01 15:29:43 +0530

    Created CustomDrawer

--- src/navigation/CustomDrawer.tsx
+++ src/navigation/CustomDrawer.tsx
@@ -1,34 +1,10 @@
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
-import {
-  DrawerContentScrollView,
-  createDrawerNavigator,
-} from '@react-navigation/drawer';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import HomeScreen from '../screens/HomeScreen';
-// import CustomDrawerContent from '../components/CustomDrawerContent';
+import CustomDrawerContent from '../components/CustomDrawerContent';
 
 const Drawer = createDrawerNavigator();
-
-const CustomDrawerContent = ({navigation}) => {
-  return (
-    <DrawerContentScrollView
-      scrollEnabled={true}
-      contentContainerStyle={{flex: 1}}>
-      <View style={{flex: 1, paddingHorizontal: 20}}>
-        {/* userDetails */}
-        <View style={{marginTop: 25}}>
-          <Image
-            source={require('../assets/profile.jpg')}
-            style={{height: 100, width: 100, borderRadius: 50}}
-          />
-        </View>
-        {/* sepetator */}
-        {/* drawerItems */}
-        {/* closeButton */}
-      </View>
-    </DrawerContentScrollView>
-  );
-};
 
 const CustomDrawer = () => {
   return (
