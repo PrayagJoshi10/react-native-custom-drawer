f7df00783d1092af1771a712058e3fbe4d888e07
Prayag Joshi <prayagjoshi263@gmail.com>
2024-03-01 15:29:18 +0530

    Created CustomDrawerContent component

--- src/components/CustomDrawerContent.tsx
+++ src/components/CustomDrawerContent.tsx
@@ -1,16 +1,39 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import React from 'react';
+import {DrawerContentScrollView} from '@react-navigation/drawer';
+import UserDetails from './UserDetails';
 
-interface Props {}
-
-const CustomDrawerContent = (props: Props) => {
+const CustomDrawerContent = ({navigation}) => {
   return (
-    <View>
-      <Text>CustomDrawerContent</Text>
-    </View>
+    <DrawerContentScrollView
+      scrollEnabled={true}
+      contentContainerStyle={styles.scrollViewContainer}>
+      <View style={styles.container}>
+        {/* userDetails */}
+        <UserDetails />
+        {/* sepetator */}
+        <View style={styles.seperator} />
+        {/* drawerItems */}
+        {/* closeButton */}
+      </View>
+    </DrawerContentScrollView>
   );
 };
 
 export default CustomDrawerContent;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  scrollViewContainer: {
+    flex: 1,
+  },
+  container: {flex: 1, paddingHorizontal: 20},
+  seperator: {
+    height: 2,
+    width: '100%',
+    backgroundColor: '#ffffff',
+    marginVertical: 20,
+  },
+  DrawerItemsContainer: {
+    flex: 1,
+  },
+});
